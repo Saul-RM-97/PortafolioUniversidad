632c8db5666cfafcbd51024248a3fd99d6b80312
Saul-RM-97 <saul_chupon@live.com.mx>
2025-12-23 02:17:06 -0600

    Solo se ajusta el texto
    Se ajusta el código para mejor entendimiento

--- Ejercicios en C++/sueldoSemanalTriple.cpp
+++ Ejercicios en C++/sueldoSemanalTriple.cpp
@@ -24,14 +24,17 @@ int main()
     else if (horasT <= 45)
     {                                                                   // Si no, Si esto es igual o menor a 45 horas trabajadas
         horasDobles = horasT - 40;                                      // Se asigna a esta variables restarle las primeras 40 horas
-        sueldoSemanal = (40 * pagoXhora + horasDobles * pagoXhora * 2); // El sueldo semanal es igual a las primeras 40 horas multiplicadas por el pago por hora + * - las horas Dobles * pago por hora por 2 (como la variable horas dobles tienen un valor negativo al multiiplicar los signos nos da -)
+        sueldoSemanal = (40 * pagoXhora + horasDobles * pagoXhora * 2); /* El sueldo semanal es igual a las primeras 40 horas multiplicadas por el pago por hora + * - las horas Dobles *
+        pago por hora por 2 (como la variable horas dobles tienen un valor negativo al multiiplicar los signos nos da -) */
     }
     else if (horasT <= 50)
     {                                                // Si horas dobles son menor a 50
         horasDobles = 45 - 40;                       // Se inicializa la variable horasDobles restando 40 horas de las primeras ingresadas
         horasTriples = horasT - 45;                  // Se inicializa la variable horastriples donde se restan las 45 horas ingresadas
         sueldoSemanal = (40 * pagoXhora + (horasDobles * pagoXhora * 2) + (horasTriples * pagoXhora * 3)); 
-        /* Se multiplican las primeras 40 horas por el pago por hora, despues se le restan las primeras 40 horas quedando asi las horas restantes las cuales se multiplican por el pago por hora y se multiplican por 2, para pagar esas horas al doble, despues se multiplica las horas triples el cual se le restan las 45 horas, por lo cual solo se va a multiplicar las horas restantes las cuales se multiplicaran por el pago por hora por 3, estas pagandosde al triple.*/
+        /* Se multiplican las primeras 40 horas por el pago por hora, despues se le restan las primeras 40 horas quedando asi las horas restantes las cuales se multiplican por el pago por hora y se 
+        multiplican por 2, para pagar esas horas al doble, despues se multiplica las horas triples el cual se le restan las 45 horas, por lo cual solo se va a multiplicar las horas restantes las cuales 
+        se multiplicaran por el pago por hora por 3, estas pagandosde al triple.*/
     }
     else {
         cout << "Trabajar mas de 50 horas no esta permitido!!" << endl;
@@ -40,4 +43,5 @@ int main()
     cout << "Las horas trabajadas fueron de: " << horasT << endl;
     cout << "El pago por hora es de: " << pagoXhora << endl;
     cout << "El sueldo semanal es de: " << sueldoSemanal << endl;
+
 }
